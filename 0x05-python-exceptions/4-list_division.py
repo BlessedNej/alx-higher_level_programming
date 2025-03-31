df15a14a01619f60fdb31345430964235f0ad540
BlessedNej <nejoblessing72@gmail.com>
2025-03-31 10:10:40 -0700

    list division

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -7,8 +7,10 @@ def list_division(my_list_1, my_list_2, list_length):
             div = my_list_1[item] / my_list_2[item]
         except TypeError:
             print("wrong type")
+            div = 0
         except ZeroDivisionError:
             print("division by 0")
+            div = 0
         except IndexError:
             print("out of range")
             div = 0
