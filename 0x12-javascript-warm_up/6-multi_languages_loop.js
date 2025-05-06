9e13a68ed23840cab6a461b61fe779242b5db58c
BlessedNej <nejoblessing72@gmail.com>
2025-05-06 07:35:08 -0700

    prints multiple lines using a loop

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -2,7 +2,7 @@
 /* a script that prints 3 lines: (like 1-multi_languages.js)
  * but by using an array of string and a loop  */
 
-const array = ['C is fun', 'python is cool', 'JavaScript is amazing'];
+const array = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 for (let i = 0; i < array.length; i++) {
   console.log(array[i]);
